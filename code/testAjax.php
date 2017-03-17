91563c071129d6f921f46b8d910a5261f4f9f7cc
Hieu Bui <cynno.uit@gmail.com>
2017-03-18 00:33:54 +0700

    ajaxxxx

--- code/testAjax.php
+++ code/testAjax.php
@@ -6,8 +6,9 @@
  * Time: 13:07
  */
 $myFile = fopen("ajax.txt", "w") or die("Unable to open file!");
-$txt = "John Doe\n";
+$now = strtotime('NOW');
+$txt = "$now \n";
 fwrite($myFile, $txt);
-$txt = "Jane Doe\n";
+$txt = "$now \n";
 fwrite($myFile, $txt);
 fclose($myFile);
